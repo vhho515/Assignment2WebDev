61215a85c7111b3d92a9aaa98d8260703476823a
vhho515 <89646443+vhho515@users.noreply.github.com>
2021-10-22 13:10:09 -0500

    Update settings.py

--- mfscrm/settings.py
+++ mfscrm/settings.py
@@ -121,8 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_URL = os.path.join(BASE_DIR, 'static')
-# LOGIN_REDIRECT_URL = '/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+LOGIN_REDIRECT_URL = '/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
